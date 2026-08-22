19589e0d74372268e88f25a97891a6a82af0e297
edouard-commits <edouard@m2mtechconnect.com>
2026-08-22 12:56:54 -0400

    refactor(dsx-reference): export source-complete corpus

--- src/data/dsxReference/index.ts
+++ src/data/dsxReference/index.ts
@@ -1,8 +1,9 @@
 export * from './types';
 export * from './facilities';
+export * from './sourceCoverage';
 export {
   DSX_REFERENCE_RECORDS,
   DSX_SOURCE_COMMIT,
   DSX_DATASET_VERSION,
   DSX_RETRIEVED_AT,
-} from './records.generated';
+} from './records';

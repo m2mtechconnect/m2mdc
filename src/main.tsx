2b015adec72ec6d11fb40ad75731c3f36fdc5283
edouard-commits <edouard@m2mtechconnect.com>
2026-08-23 16:43:19 -0400

    ui(simulation): load scoped AURA V2 simulation reset

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
+import "./styles/simulation-v2.css";
 import "./i18n/config";
 import { stampBuildFingerprint } from "./lib/buildFingerprint";
 
